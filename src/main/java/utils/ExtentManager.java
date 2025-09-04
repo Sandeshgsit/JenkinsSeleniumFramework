063a13aebf52fcf1e128dee424305458fb2e8a39
Sandesh <sandeshgsit>
2025-09-04 21:58:44 +0530

    Push

--- src/main/java/utils/ExtentManager.java
+++ src/main/java/utils/ExtentManager.java
@@ -7,7 +7,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentManager {
-    private static ExtentReports extent;   
+    private static ExtentReports extent;      
 
     public static ExtentReports getInstance() {
         if (extent == null) {
